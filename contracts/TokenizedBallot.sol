2e4f8febd3c0ebd17e00638b5aeb76e66f26651e
Elliot Mangini <elliot.mangini@gmail.com>
2022-12-05 13:07:57 -0700

    i think everything is in place

--- contracts/TokenizedBallot.sol
+++ contracts/TokenizedBallot.sol
@@ -1,9 +1,68 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
+interface IVoteToken {
+    // things we want to be able to call from our vote token
+    // We can add view to save gas since this is a read-only call to the blockchain.
+    function getPastVotes(address, uint256) external view returns (uint256);
 
 
-contract TokenizedBallot {
+}
 
+contract TokenizedBallot {    
+    // We don't need chairpersons or voters with a self-delegating vote system.
+    // Just proposals, an interface for our vote token, and proposals and queries.
 
+    struct Proposal {
+        bytes32 name;   
+        uint voteCount; 
+    }
+
+    IVoteToken public tokenContract;
+    Proposal[] public proposals;
+    uint256 public targetBlockNumber;
+
+    mapping (address => uint256) public votePowerSpent;
+    
+    // Sets the contracts internal information at the time it is deployed.
+    constructor(
+        bytes32[] memory proposalNames,
+        address _tokenContract,
+        uint256 _targetBlockNumber
+    ) {
+        tokenContract = IVoteToken(_tokenContract);
+        targetBlockNumber = _targetBlockNumber;
+        for (uint i = 0; i < proposalNames.length; i++) {
+            proposals.push(Proposal({name: proposalNames[i], voteCount: 0 }));
+        }
+    }
+    
+    // We add an amount arg so you can use some votes for one thing and some for another
+    function vote(uint proposal, uint256 amount) external {
+        require(votingPower(msg.sender) >= amount, "TokenizedBallot: Insufficient Votes");
+        votePowerSpent[msg.sender] += amount;
+        proposals[proposal].voteCount += amount;
+    }
+
+    function votingPower(address account) public view returns (uint256) {
+        return tokenContract.getPastVotes(account, targetBlockNumber) - votePowerSpent[account];
+    }
+    
+    function winningProposal() public view
+            returns (uint winningProposal_)
+    {
+        uint winningVoteCount = 0;
+        for (uint p = 0; p < proposals.length; p++) {
+            if (proposals[p].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[p].voteCount;
+                winningProposal_ = p;
+            }
+        }
+    }
+    
+    function winnerName() external view
+            returns (bytes32 winnerName_)
+    {
+        winnerName_ = proposals[winningProposal()].name;
+    }
 }
--- scripts/ERC20Votes.ts
+++ scripts/ERC20Votes.ts
@@ -1,4 +1,5 @@
 import { ethers } from "hardhat";
+// import { contracts } from "../typechain-types";
 import { VoteToken__factory } from "../typechain-types/factories/contracts/ERC20Votes.sol/VoteToken__factory";
 
 const MINT_VALUE = ethers.utils.parseEther("6")
@@ -14,7 +15,7 @@ async function main() {
     //Mint tokens to accounts[1], the minter is accounts[0] automatically cause thats the deployer.
     const mintTx = await voteContract.mint(accounts[1].address, MINT_VALUE);
     await mintTx.wait();
-    console.log(`Minted ${MINT_VALUE.toString()} decimal units to account ${accounts[1]}.`)
+    console.log(`Minted ${MINT_VALUE.toString()} decimal units to account 1: ${accounts[1].address}.`)
 
     // Check balance
     const balanceBN = await voteContract.balanceOf(accounts[1].address);
@@ -28,11 +29,34 @@ async function main() {
     const delegateTx = await voteContract.connect(accounts[1]).delegate(accounts[1].address);
     await delegateTx.wait();
     const votePowerAfterDelegation = await voteContract.getVotes(accounts[1].address);
-    console.log(`voting power of accounts[1] after delegation is ${votePowerAfterDelegation}`);
+    console.log(`voting power of accounts[1] after self-delegation is ${votePowerAfterDelegation}`);
 
+    // Transfer tokens out
+    console.log(`transfering ${MINT_VALUE.div(3)} tokens from accounts[1] to accounts[2]`);
+    const transferTx = await voteContract.connect(accounts[1]).transfer(accounts[2].address, MINT_VALUE.div(3))
+    await transferTx.wait();
+    const votePower2AfterTransfer = await voteContract.getVotes(accounts[2].address);
+    console.log(`voting power of accounts[2] after receiving votes is ${votePower2AfterTransfer}`);
+    const votePower1AfterTransfer = await voteContract.getVotes(accounts[1].address);
+    console.log(`voting power of accounts[1] after giving away votes is ${votePower1AfterTransfer}`);
+
+    // self-delegate
+    const delegate2Tx = await voteContract.connect(accounts[2]).delegate(accounts[2].address);
+    await delegate2Tx.wait();
+    const votePower2AfterDelegation = await voteContract.getVotes(accounts[2].address);
+    console.log(`voting power of accounts[2] after self-delegation is ${votePower2AfterDelegation}`);
+
+    // Check past voting power
+    const lastBlock = await ethers.provider.getBlock("latest");
+    console.log(`Current block number is ${lastBlock.number}`);
+    const pastVotes = await voteContract.getPastVotes(accounts[1].address, lastBlock.number = 1);
+    console.log(`Account 1: ${accounts[1].address} has ${pastVotes.toString()} units of voting power from the previous block.`)
 }
 
 main().catch((error) => {
     console.error(error);
     process.exitCode = 1;
 })
+
+// TO DO
+// checking vote power and querying results (we do in the ballot)
